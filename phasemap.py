44ec6ac5443c22230b4ebb1f115988a074541629
Dominik Gresch <greschd@gmx.ch>
2016-08-29 02:50:28 +0200

    added item access to data container

--- phasemap.py
+++ phasemap.py
@@ -15,16 +15,25 @@ __version__ = '0.0.0a1'
 
 PhaseResult = namedtuple('PhaseResult', ['phase', 'virtual'])
 
+@export
 class PhaseMap:
     """data container"""
     def __init__(self, mesh, limits):
+        """
+        mesh is a list of integers
+        limits is a list of tuples
+        """
         if len(mesh) != len(limits):
             raise ValueError('inconsistent dimensions for mesh ({}) and limits ({})'.format(mesh, limits))
-        self.limits = tuple(limits)
+        self.limits = limits
         self.mesh = np.empty(mesh, dtype=object)
         
+    def __getitem__(self, *args):
+        return self.mesh.__getitem__(*args)
+
     def extend(self):
         """increases mesh density"""
+        pass
         
     def indices(self):
         """Returns an iterator over the indices in the mesh."""
@@ -49,7 +58,9 @@ class PhaseMap:
         
     def index_to_position(self, idx):
         """Returns the position on the phase map corresponding to a given index."""
+        pass
     
+@export
 def get_phase_map(fct, limits, init_mesh=5, num_steps=15):
     """
     init_mesh as int -> same in all dimensions. Otherwise as list of int.
