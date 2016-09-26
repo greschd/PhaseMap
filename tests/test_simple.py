7b0f21a884005cd4e8ba3e67fc9103d7ff828f51
Dominik Gresch <greschd@gmx.ch>
2016-09-27 01:48:29 +0200

    added 3D test

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -11,14 +11,14 @@ import pytest
 import numpy as np
 import phasemap as pm
 
-def circle(x, y):
-    return 2 if x**2 + y**2 < 1 else 0
+def circle(x, y, z=0):
+    return 2 if x**2 + y**2 + z**2 < 1 else 0
 
-def line(x, y):
+def line(x, y, z=0):
     return 1 if x > 0.5 or y < 0.2 else 0
 
 def phase1(val):
-    return [line(x, y) + circle(x, y) for x, y in val]
+    return [line(*v) + circle(*v) for v in val]
     
 def phase2(pos):
     if pos[0] >= 0 and pos[0] < 0.3:
@@ -37,26 +37,26 @@ def phase2(pos):
             return 1
 
     return 0
-    
-def phase3(pos):
-    x, y = pos
-    if y > 0.8 and x < 0.6:
-        return 1
-    return 0
-
 
 @pytest.mark.parametrize('num_steps', range(2, 5))
 @pytest.mark.parametrize('all_corners', [False, True])
-@pytest.mark.parametrize('phase,listable', [(phase1, True), (phase2, False)])
-def test_phase(compare_equal, num_steps, all_corners, phase, listable):
-    res = pm.get_phase_map(phase, [(-1, 1), (-1, 1)], num_steps=num_steps, init_mesh=3, all_corners=all_corners, listable=listable)
+@pytest.mark.parametrize('phase, listable, limits', [(phase1, True, [(-1, 1), (-1, 1)]), (phase2, False, [(0, 1), (0, 1)])])
+def test_phase(compare_equal, num_steps, all_corners, phase, listable, limits):
+    res = pm.get_phase_map(phase, limits, num_steps=num_steps, init_mesh=3, all_corners=all_corners, listable=listable)
+    compare_equal(sorted([(k, v.phase) for k, v in res.points.items()]))
+
+@pytest.mark.parametrize('num_steps', range(0, 3))
+@pytest.mark.parametrize('all_corners', [False, True])
+@pytest.mark.parametrize('phase, listable, limits', [(phase1, True, [(-1, 1), (-1, 1), (-1, 1)])])
+def test_3d(compare_equal, num_steps, all_corners, phase, listable, limits):
+    res = pm.get_phase_map(phase, limits=limits, num_steps=num_steps, init_mesh=3, all_corners=all_corners, listable=listable)
     compare_equal(sorted([(k, v.phase) for k, v in res.points.items()]))
 
 #~ @pytest.mark.parametrize('num_steps', range(2, 5))
 #~ @pytest.mark.parametrize('all_corners', [False, True])
 #~ @pytest.mark.parametrize('phase,listable', [(phase1, True), (phase2, False)])
-def test_same_square_idx(compare_equal):
-    res = pm.get_phase_map(phase3, [(0, 1), (0, 1)], num_steps=2, init_mesh=2, listable=False)
-    compare_equal(sorted([(k, v.phase) for k, v in res.points.items()]))
+#~ def test_same_square_idx(compare_equal):
+    #~ res = pm.get_phase_map(phase3, [(0, 1), (0, 1)], num_steps=2, init_mesh=2, listable=False)
+    #~ compare_equal(sorted([(k, v.phase) for k, v in res.points.items()]))
     # This can be used when the code is stable and io is done
     #~ compare_result(res)
