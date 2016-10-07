2fc5950c382d490df118d93f07736bb121bc12e1
Dominik Gresch <greschd@gmx.ch>
2016-10-07 16:48:51 +0200

    linting

--- phasemap/plot.py
+++ phasemap/plot.py
@@ -38,7 +38,7 @@ def _plot(func, phase_map, *, fig=None, ax=None, add_cbar=True, **kwargs):
     if add_cbar:
         if fig is None:
             raise ValueError('Colorbar cannot be set when the ax is given explicitly, but the figure is not.')
-        
+
         fig.subplots_adjust(right=0.9)
         cbar_ax = fig.add_axes([0.95, 0.1, 0.04, 0.8])
 
@@ -49,7 +49,7 @@ def _plot(func, phase_map, *, fig=None, ax=None, add_cbar=True, **kwargs):
             [cmap(v) for v in color_vals]
         )
         c_bar = ColorbarBase(
-            cbar_ax, 
+            cbar_ax,
             cmap=cbar_cmap,
             norm=norm,
             ticks=np.linspace(min(vals), max(vals), 2 * len(vals) + 1)[1::2],
@@ -64,46 +64,42 @@ def _plot(func, phase_map, *, fig=None, ax=None, add_cbar=True, **kwargs):
 def squares(
         phase_map,
         *,
-        ax=None, 
-        log_norm=False, 
+        ax=None,
+        log_norm=False,
         scale_val=None,
         cmap=None,
         fill_lines=False,
         **kwargs
     ):
-    
+
     if cmap is None:
         # don't do this in the signature, otherwise it gets set at import time
         cmap = plt.get_cmap()
-    
-    squares = [s for s in phase_map.squares if s.phase is not None]
-    vals = [s.phase for s in squares]
+
+    sqrs = [s for s in phase_map.squares if s.phase is not None]
+    vals = [s.phase for s in sqrs]
     all_vals = sorted(set(vals))
-    
-    
-    if log_norm:
-        norm = LogNorm()
-    else:
-        norm = Normalize()
+
+    norm = LogNorm() if log_norm else Normalize()
     if scale_val is None:
         norm.autoscale(all_vals)
     else:
         norm.autoscale(scale_val)
 
     colors = cmap([norm(v) for v in vals])
-    
+
     rect_properties = ChainMap(
-        kwargs, 
+        kwargs,
         dict(lw=1e-11 if fill_lines else 0.)
     )
-    for c, s in zip(colors, squares):
+    for c, s in zip(colors, sqrs):
         ax.add_patch(Rectangle(
             xy=s.corner,
             width=s.size,
             height=s.size,
             **ChainMap(rect_properties, dict(facecolor=c, edgecolor=c))
         ))
-        
+
     return ax, cmap, norm, all_vals
 
 @export
@@ -111,8 +107,8 @@ def squares(
 def points(
         phase_map,
         *,
-        ax=None, 
-        log_norm=False, 
+        ax=None,
+        log_norm=False,
         scale_val=None,
         cmap=None,
         **kwargs
@@ -120,29 +116,26 @@ def points(
     if cmap is None:
         # don't do this in the signature, otherwise it gets set at import time
         cmap = plt.get_cmap()
-    
-    points = phase_map.points
-    all_vals = sorted(set([p.phase for p in points.values()]))
 
-    if log_norm:
-        norm = LogNorm()
-    else:
-        norm = Normalize()
+    pts = phase_map.points
+    all_vals = sorted(set([p.phase for p in pts.values()]))
+
+    norm = LogNorm() if log_norm else Normalize()
     if scale_val is None:
         norm.autoscale(all_vals)
     else:
         norm.autoscale(scale_val)
-    
-    
+
+
     point_colors = defaultdict(list)
-    for p, v in points.items():
+    for p, v in pts.items():
         point_colors[cmap(norm(v.phase))].append(p)
 
-    for color, pts in point_colors.items():
+    for c, p in point_colors.items():
         ax.scatter(
-            [p[0] for p in pts],
-            [p[1] for p in pts],
-            color=color,
+            [pt[0] for pt in p],
+            [pt[1] for pt in p],
+            color=c,
             **kwargs
         )
 
