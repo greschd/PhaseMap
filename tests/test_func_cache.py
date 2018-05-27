e03d65355f0b38c285af996429fded302f803e1c
Dominik Gresch <greschd@gmx.ch>
2018-05-28 00:05:24 +0200

    Fix func_cache test.

--- tests/test_func_cache.py
+++ tests/test_func_cache.py
@@ -1,5 +1,6 @@
+import asyncio
+
 import pytest
-import numpy as np
 
 from phasemap._cache import FuncCache
 
@@ -8,16 +9,24 @@ def echo(x):
     return x
 
 
+async def echo_async(x):
+    return x
+
+
 def error(x):
     raise ValueError(x)
 
 
-def test_func_cache():
-    func = FuncCache(echo)
-    for x in range(10):
-        assert x == func(x)
-    func_error = FuncCache(error, data=func.data)
-    for x in range(10):
-        assert x == func_error(x)
-    with pytest.raises(ValueError):
-        func_error(10)
+@pytest.mark.parametrize('func', [echo, echo_async])
+def test_func_cache(func):
+    async def run():
+        func_cache = FuncCache(func)
+        for x in range(10):
+            assert x == await func_cache(x)
+        func_error = FuncCache(error, data=func_cache.data)
+        for x in range(10):
+            assert x == await func_error(x)
+        with pytest.raises(ValueError):
+            await func_error(10)
+
+    asyncio.get_event_loop().run_until_complete(run())
