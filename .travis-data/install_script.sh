1fff6c7bf91b2dd2741fc5dd6c22923ac98e8ae5
Dominik Gresch <a-dogres@microsoft.com>
2020-10-02 12:34:18 +0200

    Update setuptools in Travis install.

--- .travis-data/install_script.sh
+++ .travis-data/install_script.sh
@@ -7,6 +7,7 @@ set -ev
 
 cd ${TRAVIS_BUILD_DIR}
 
+
 case "$INSTALL_TYPE" in
     dev)
         pip install .[dev]
@@ -15,6 +16,7 @@ case "$INSTALL_TYPE" in
         pip install .[dev]
         ;;
     test_sdist)
+        pip install -U setuptools
         python setup.py sdist
         ls -1 dist/ | xargs -I % pip install dist/%[dev]
         ;;
